b1b1f2f56a73f21c0e831de73c86f341c5a6d97d
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-12-16 00:18:51 -0500

    push to my github please

--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -7,7 +7,7 @@ namespace SchoolPractice
 {
     public class Student
     {
-        private string Name { get, set};
+        private string Name;
         private int StudentId;
         private int NumberOfCredits;
         private double Gpa;
